e916d272de252095adb19fa35cd69f77eef120d6
2012398 <bscs2012398@szabist.pk>
2023-10-02 04:51:20 +0500

    My app

---
+++ lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:fyp/splash.dart';
+
+void main() {
+  runApp(FypApp());
+}
+
+class FypApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "Babybloom",
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: SplashScreen(),
+    );
+  }
+}
---
+++ lib/splash.dart
@@ -0,0 +1,72 @@
+import 'dart:async';
+import 'package:flutter/material.dart';
+import 'package:fyp/onboarding_screen.dart';
+
+class SplashScreen extends StatefulWidget {
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    // Timer to display on screen
+    super.initState();
+    Timer(Duration(seconds: 2), () {
+      // push navigator to display another screen on stack
+      Navigator.pushReplacement(
+          context,
+          MaterialPageRoute(
+            builder: (context) => Onboarding_Screen(),
+          ));
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(
+        width: double.infinity,
+        height: double.infinity,
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            colors: [Color(0xFF6392C9), Color(0x008EA0E7)],
+
+            //   x cordinates: 0,0(up side)  1,0(upside)
+            //   y coordinates:0,1(down side) 1,1(downside)
+            begin: Alignment(0.00, -1.00),
+            end: Alignment(0, 1),
+          ),
+        ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Column(
+              children: [
+                Image.asset(
+                  'assets/images/Baby_bloom.png',
+                  width: 85,
+                  height: 85,
+                ),
+                const SizedBox(
+                  height: 15,
+                ),
+                Text(
+                  "BabyBloom",
+                  style: TextStyle(
+                      fontSize: 20,
+                      fontFamily: 'Montserrat',
+                      fontWeight: FontWeight.w900,
+                      height: 0,
+                      letterSpacing: -0.30,
+                      color: Colors.black.withOpacity(0.75999)),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
